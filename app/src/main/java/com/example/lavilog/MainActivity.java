8a558263c9f295ce14a19b37e1b827aeed619b64
harrison <kill60673@gmail.com>
2019-09-28 15:18:00 +0800

    中燕很害羞

--- app/src/main/java/com/example/lavilog/MainActivity.java
+++ app/src/main/java/com/example/lavilog/MainActivity.java
@@ -10,8 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //
-        //母湯哲 禎
+        //ii忠諺在害羞
 
     }
 }
