fa3d24efecfff956824d510168a9e71b0461c48a
張哲禎 <tkotw1988@gmail.com>
2019-09-29 17:42:59 +0800

    哲禎 更新layout

--- app/src/main/java/com/example/lavilog/signUpFragment.java
+++ app/src/main/java/com/example/lavilog/signUpFragment.java
@@ -9,17 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class signUpFragment extends Fragment {
-
-
-    public signUpFragment() {
-        // Required empty public constructor
-    }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
