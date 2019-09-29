e4bc4db8e752789f4261bccd59e94c4aa6ca3454
張哲禎 <tkotw1988@gmail.com>
2019-09-29 16:40:39 +0800

    哲楨  測試

--- app/src/main/java/com/example/lavilog/MainActivity.java
+++ app/src/main/java/com/example/lavilog/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //ii忠諺在害羞
+        //testjhe
 
     }
 }
