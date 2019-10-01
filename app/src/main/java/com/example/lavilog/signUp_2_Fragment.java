ac9ffb7fd99c8bb1540e0abc2e726ccb62c745a4
tkotw1988g <tkotw1988@gmail.com>
2019-10-01 17:26:30 +0800

    哲禎
    signUp_2_

---
+++ app/src/main/java/com/example/lavilog/signUp_2_Fragment.java
@@ -0,0 +1,31 @@
+package com.example.lavilog;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class signUp_2_Fragment extends Fragment {
+
+
+    public signUp_2_Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_up_2_, container, false);
+    }
+
+}
