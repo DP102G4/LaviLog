615959259a38880ef27331cba2b5101ca8ced67a
tkotw1988g <tkotw1988@gmail.com>
2019-10-02 16:55:13 +0800

    哲禎 製作signUp第三頁

--- app/src/main/java/com/example/lavilog/signUp_1_Fragment.java
+++ app/src/main/java/com/example/lavilog/signUp_1_Fragment.java
@@ -7,15 +7,18 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.google.firebase.auth.FirebaseAuth;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class signUp_1_Fragment extends Fragment {
     private Activity activity;
+    Button btConfirm;
+    TextView tvtvHaveAccount;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -34,6 +37,13 @@ public class signUp_1_Fragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        btConfirm=view.findViewById(R.id.btConfirm3);
+        btConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_signUp_1_Fragment_to_signUp_2_Fragment);
+            }
+        });
     }
 
 
--- app/src/main/java/com/example/lavilog/signUp_3_Fragment.java
+++ app/src/main/java/com/example/lavilog/signUp_3_Fragment.java
@@ -1,25 +1,32 @@
 package com.example.lavilog;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class signUp_3_Fragment extends Fragment {
+    Activity acitvity;
+    EditText etAccount,etPassword,etRePassword,etName;
+    TextView tvStatus_Account,tvStatus_Password,tvStatus_Name,tvPhone;
+    Button btConfirm,btBack;
 
-
-    public signUp_3_Fragment() {
-        // Required empty public constructor
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        acitvity=getActivity();
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,4 +35,51 @@ public class signUp_3_Fragment extends Fragment {
         return inflater.inflate(R.layout.fragment_sign_up_3_, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        etAccount=view.findViewById(R.id.etAccount);
+        etPassword=view.findViewById(R.id.etPassword);
+        etRePassword=view.findViewById(R.id.etRePassword);
+        etName=view.findViewById(R.id.etName);
+        tvStatus_Account=view.findViewById(R.id.tvStatus_Account);
+        tvStatus_Password=view.findViewById(R.id.tvStatus_Password);
+        tvStatus_Name=view.findViewById(R.id.tvStatus_Name);
+        tvPhone=view.findViewById(R.id.tvPhone);
+        btConfirm=view.findViewById(R.id.btConfirm3);
+        btBack=view.findViewById(R.id.btBack);
+        Bundle bundle =getArguments();
+        String phone=(String)bundle.getSerializable("phone");
+        tvPhone.setText(phone);
+        btBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_signUp_3_Fragment_to_signInFragment);
+            }
+        });
+        btConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String account=etAccount.getText().toString().trim();
+                String password=etPassword.getText().toString().trim();
+                String rePassword=etRePassword.getText().toString().trim();
+                String name=etName.getText().toString().trim();
+                if(!account.contains("@")){
+                    tvStatus_Account.setText("帳號應為信箱格式");
+                    return;
+                }else{
+                    tvStatus_Account.setText("");
+                }
+                if(!password.equals(rePassword)){
+                    tvStatus_Password.setText("兩次密碼輸入不同，請重新輸入");
+                    return;
+                }else{
+                    tvStatus_Password.setText("");
+                }
+                if(name.length()>5){
+                    tvStatus_Name.setText("姓名長度不得大於5個字");
+                }
+            }
+        });
+    }
 }
