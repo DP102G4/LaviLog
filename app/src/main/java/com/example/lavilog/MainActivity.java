b3437709f5b662f064f2d295c6dc4b74b4eb081f
Yi_Nan <kekecccccc@gmail.com>
2019-10-07 12:00:33 +0800

    1007 乙男：Firebase v6

--- app/src/main/java/com/example/lavilog/MainActivity.java
+++ app/src/main/java/com/example/lavilog/MainActivity.java
@@ -26,9 +26,6 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationView = findViewById(R.id.navigation);
         NavController navController = Navigation.findNavController(this,R.id.nav_host_fragment);
         NavigationUI.setupWithNavController(bottomNavigationView,navController);
-
-
-
     }
 
     @Override
