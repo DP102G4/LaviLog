9fe48c9d9fd7e0cd384677a60b1d6e0413edc21a
Kao <aboutkao0807@gmail.com>
2019-10-07 09:48:20 +0800

    1004 Kao 第三次整合(firebase 上傳)

--- app/src/main/java/com/example/lavilog/Daily/Answer.java
+++ app/src/main/java/com/example/lavilog/Daily/Answer.java
@@ -5,9 +5,19 @@ import java.io.Serializable;
 
 public class Answer implements Serializable {
     private String article;
+    private String imagePath;
+    private String textClock;
+
 
     public Answer() { //先不傳值所以為空
     }
+
+    public Answer(String article, String imagePath, String textClock) {
+        this.article = article;
+        this.imagePath = imagePath;
+        this.textClock = textClock;
+    }
+
 
     public String getArticle() {
         return article;
@@ -17,8 +27,21 @@ public class Answer implements Serializable {
         this.article = article;
     }
 
-    public Answer(String article) {
-        this.article = article;
+    public String getImagePath() {
+        return imagePath;
     }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getTextClock() {
+        return textClock;
+    }
+
+    public void setTextClock(String textClock) {
+        this.textClock = textClock;
+    }
+
 }
 
