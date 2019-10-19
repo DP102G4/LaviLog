1568f619bcc94fbbd3a85ce85e440130982f357a
Kao <aboutkao0807@gmail.com>
2019-10-19 16:23:55 +0800

    1014 Kao 刪serach 日誌排序

--- app/src/main/java/com/example/lavilog/Daily/Answer.java
+++ app/src/main/java/com/example/lavilog/Daily/Answer.java
@@ -2,6 +2,7 @@ package com.example.lavilog.Daily;
 
 
 import java.io.Serializable;
+import java.time.Year;
 
 public class Answer implements Serializable {
     private String question;
@@ -9,17 +10,25 @@ public class Answer implements Serializable {
     private String imagePath;
     private String textClock;
     private String id;
+    private String year;
+    private String month;
+    private String day;
+    private String account;
 
 
     public Answer() { //先不傳值所以為空
     }
 
-    public Answer(String question,String answer, String imagePath, String textClock,String id) {
+    public Answer(String question, String answer, String imagePath, String textClock, String id, String year, String month, String day, String account) {
         this.question = question;
         this.answer = answer;
         this.imagePath = imagePath;
         this.textClock = textClock;
         this.id = id;
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        this.account = account;
     }
 
     public String getQuestion() {
@@ -60,6 +69,38 @@ public class Answer implements Serializable {
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
     }
 }
 
